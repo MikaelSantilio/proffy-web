78b1a810ab010883f1026c3fbe26426c479cbd4d
MikaelSantilio <mikael.santilio@gmail.com>
2020-08-08 20:46:42 -0300

    connecting and rendering the TeacherList with the API router

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,25 +3,41 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 import './styles.css';
 
 
-function TeacherItem() {
+interface ProffyUser {
+    id: number;
+    avatar: string;
+    bio: string;
+    name: string;
+    whatsapp: string;
+}
+
+export interface Teacher {
+    proffy_user: ProffyUser;
+    cost: number;
+    subject: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher; 
+}
+
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
     return (
         <article className="teacher-item" >
             <header>
-                <img src="https://avatars1.githubusercontent.com/u/40041499?s=460&u=b484cfea7185c43f1a07cc8ba3a75a82cdc20b27&v=4" alt="Perfil"/>
+                <img src={ teacher.proffy_user.avatar } alt="Perfil"/>
                 <div>
-                    <strong>Mikael Santilio</strong>
-                    <span>Filosofia</span>
+                    <strong>{ teacher.proffy_user.name }</strong>
+                    <span>{ teacher.subject }</span>
                 </div>
             </header>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fermentum quis ipsum eu egestas. 
-            Maecenas molestie sapien ut leo vehicula varius. Aenean egestas, mauris nec auctor pharetra, 
-            lectus metus posuere tortor, eu finibus mauris mi quis dolor.
-            </p>
+            <p>{ teacher.proffy_user.bio }</p>
+
             <footer>
                 <p>
                     Preço/Hora
-                    <strong>USD 100,00</strong>
+                    <strong>R$ { teacher.cost }</strong>
                 </p>
 
                 <button type="button" >
