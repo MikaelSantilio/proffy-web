003a915f86a1e112ddfb063cda08c3cf2af385e7
MikaelSantilio <mikael.santilio@gmail.com>
2020-08-08 20:02:37 -0300

    redirecting on teacher register

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, FormEvent } from 'react';
+import { useHistory } from 'react-router-dom';
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 import warningIcon from '../../assets/images/icons/warning.svg';
@@ -8,6 +9,7 @@ import Select from '../../components/Select';
 import api from '../../services/api';
 
 function TeacherForm() {
+    const history = useHistory();
 
     const [name, setName] = useState('');
     const [avatar, setAvatar] = useState('');
@@ -50,14 +52,13 @@ function TeacherForm() {
             cost: Number(cost),
             schedule: scheduleItems
         }).then(() => {
-            alert('Cadastro realizado com sucesso!');
+            history.push('/');
         }).catch((function (error) {
             if (error.response) {
                 console.log(error.response.data);
                 console.log(error.response.status);
                 console.log(error.response.headers);
               }
-        
           }));
     }
 
