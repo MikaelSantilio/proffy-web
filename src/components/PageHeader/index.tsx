1f725e574c353e41b3ba1c928671c8b9d0d3eb19
MikaelSantilio <mikael.santilio@gmail.com>
2020-08-08 11:16:16 -0300

    marking the teacher form

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -6,11 +6,12 @@ import backIcon from '../../assets/images/icons/back.svg';
 
 interface PageHeaderProps {
     title: string;
+    description?: string | null;
 }
 // dentro dos parenteses ( props ) ou
 // com a abertura do objeto ( { propriedades_aqui } )
 // children é uma propriedade automatica do react para o centro dentro da tag jsx
-const PageHeader: React.FunctionComponent<PageHeaderProps> = ({ title, children }) => {
+const PageHeader: React.FunctionComponent<PageHeaderProps> = ({ title, children, description }) => {
     return (
         <header className="page-header">
             <div className="top-bar-container">
@@ -21,7 +22,8 @@ const PageHeader: React.FunctionComponent<PageHeaderProps> = ({ title, children 
             </div>
             
             <div className="header-content">
-                <strong>{ title }</strong> 
+                <strong>{ title }</strong>
+                { description && <p>{ description }</p> }
             </div>
             {children}
         </header>
--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -4,7 +4,31 @@ import PageHeader from '../../components/PageHeader';
 function TeacherForm() {
     return (
         <div id="page-teacher-form" className="container">
-            <PageHeader title="alakdjsadlajdlsjadldasdklaskdjsaklkdlasdka" />
+            <PageHeader 
+            title="Que incrível que você quer dar aulas."
+            description="O primeiroé preencher esse formulário de inscrição"
+            />
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+
+                    <div className="input-block" >
+                        <label htmlFor="name">Nome completo</label>
+                        <input type="text" name="name" id="name"/>
+                    </div>
+
+                    <div className="input-block" >
+                        <label htmlFor="avatar">Avatar</label>
+                        <input type="text" name="avatar" id="avatar"/>
+                    </div>
+
+                    <div className="input-block" >
+                        <label htmlFor="whatsapp">Whatsapp</label>
+                        <input type="text" name="whatsapp" id="whatsapp"/>
+                    </div>
+                </fieldset>
+            </main>
         </div>
     );
 }
